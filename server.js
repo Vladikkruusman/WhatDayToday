12e1491fa05fa93e9ce62903edc9d620a0d56c9d
Opilane TTHK <opilane@tthk.ee>
2020-09-30 09:23:38 +0300

    WhatDayToday

--- server.js
+++ server.js
@@ -2,11 +2,20 @@ const express = require('express');
 const ejs = require('ejs');
 const app = express();
 
+app.use(express.static("public"));
 app.set('view engine', ejs);
+
 
 app.get('/date',(req,res)=>{
     let today =new Date();
     let day ='';
+    let personData = {
+                firstName: "John",
+                lastName: "Doe",
+                age: 34,
+                job: "Developer"
+                };
+                
 
     if(today.getDay() === 6 || today.getDate()=== 0){
         day="Weekend!";
@@ -14,7 +23,9 @@ app.get('/date',(req,res)=>{
         day = "Workday";
     }
     
-    res.render('index.ejs', {kindOfDay: day});
+    res.render('index.ejs', {kindOfDay: day,
+                            person: personData
+                            });
 });
 
 app.listen(5000,()=>{
